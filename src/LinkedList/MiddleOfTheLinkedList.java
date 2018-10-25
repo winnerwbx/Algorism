5a2b97cb61792eb011c550a8d5d01028e8c0caa8
wangboxuan <wangboxuan@xiaomi.com>
2018-10-25 10:31:25 +0800

    [876] Middle Of The Linked List

---
+++ src/LinkedList/MiddleOfTheLinkedList.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+public class MiddleOfTheLinkedList {
+    /**
+     * Definition for singly-linked list.
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode ahead = dummy;
+            ListNode behind = dummy;
+            while (behind != null) {
+                ahead = ahead.next;
+                if (behind.next == null) {
+                    break;
+                }
+                behind = behind.next.next;
+            }
+            return ahead;
+        }
+    }
+
+}
