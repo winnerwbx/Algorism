b9decad95ab79ca8d5886d5e642d70d21aa7fbe5
wangboxuan <wangboxuan@meituan.com>
2019-05-28 13:45:10 +0800

    add dp and bit

--- src/bit/NQueens.java
+++ src/bit/NQueens.java
@@ -49,3 +49,39 @@ class Solution {
         }
     }
 }
+
+/**
+ * 51. N-Queens
+ * <p>
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+ * <p>
+ * <p>
+ * <p>
+ * Given an integer n, return all distinct solutions to the n-queens puzzle.
+ * <p>
+ * Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 4
+ * Output: [
+ * [".Q..",  // Solution 1
+ * "...Q",
+ * "Q...",
+ * "..Q."],
+ * <p>
+ * ["..Q.",  // Solution 2
+ * "Q...",
+ * "...Q",
+ * ".Q.."]
+ * ]
+ * Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+ */
+class Solution {
+
+    public List<List<String>> solveNQueens(int n) {
+        if (n < 1) {
+            return Collections.emptyList();
+        }
+    }
+}
